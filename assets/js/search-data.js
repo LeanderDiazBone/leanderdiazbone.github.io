2ea3b03d0f1152e7d73fc29f27a479c2f798c95c
LeanderDiazBone <43293273+LeanderDiazBone@users.noreply.github.com>
2025-10-04 12:00:14 +0000

    Deploying to gh-pages from @ LeanderDiazBone/leanderdiazbone.github.io@2fb5e8f33ee521c79af16a0cc74cdc01318081c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/curriculum_vitea.pdf";
           },
         },{id: "news-i-am-happy-to-share-that-discover-automated-curricula-for-sparse-reward-reinforcement-learning-has-been-accepted-at-neurips-2025",
           title: 'I am happy to share that DISCOVER: Automated Curricula for Sparse-Reward Reinforcement Learning...',
